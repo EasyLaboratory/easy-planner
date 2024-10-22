e9f86d9c867db673158127688ed8421395ea51c1
XingjieFu <fuxingjie1998@163.com>
2024-10-22 10:48:33 +0800

    clear code

--- src/planning/planning/src/planning_nodelet.cpp
+++ src/planning/planning/src/planning_nodelet.cpp
@@ -110,7 +110,6 @@ class Nodelet : public nodelet::Nodelet {
     point.velocity.y = vel.y();
     point.velocity.z = vel.z();
     normalizeYaw(&yaw);  // Normalize yaw
-    // 这个到时候看看要不要加不加
     point.yaw = yaw;
     ROS_INFO(
         "kinematic to airsim point (x, y, z) = (%f, %f, %f), (vx, vy, vz) = "
@@ -150,9 +149,9 @@ class Nodelet : public nodelet::Nodelet {
     marker.pose.orientation.w = 1.0;  // 标准姿态
 
     // 设置 Marker 的大小
-    marker.scale.x = 5;  // 半径
-    marker.scale.y = 5;
-    marker.scale.z = 5;
+    marker.scale.x = 2;  // 半径
+    marker.scale.y = 2;
+    marker.scale.z = 2;
 
     // 设置颜色
     marker.color.r = 0.0f;
@@ -284,8 +283,6 @@ class Nodelet : public nodelet::Nodelet {
     if (!target_received_) {
       return;
     }
-    ROS_INFO("force_hover_ = %s", force_hover_ ? "true" : "false");
-    // NOTE obtain state of target
     // 只要target是锁的状态就不动
     while (target_lock_.test_and_set());
     replanStateMsg_.target = target_msg_;
@@ -303,11 +300,6 @@ class Nodelet : public nodelet::Nodelet {
     target_q.y() = replanStateMsg_.target.pose.pose.orientation.y;
     target_q.z() = replanStateMsg_.target.pose.pose.orientation.z;
 
-    // NOTE force-hover: waiting for the speed of drone small enough
-    ROS_INFO("odom_v.norm() = %f", odom_v.norm());
-    // if (force_hover_ && odom_v.norm() > 0.1) {
-    //   return;
-    // }
     // todo 0812 定高飞行，记得修改，原来是1.0
     target_p.z() += relative_height_;
     // NOTE determin whether to replan
@@ -316,25 +308,9 @@ class Nodelet : public nodelet::Nodelet {
     // TODO FXJ 在实际的飞机上需要修改!!!!
     double desired_yaw = std::atan2(dp.y(), dp.x()) - M_PI / 2.0;
     ROS_INFO("desired_yaw = %f", desired_yaw);
-    Eigen::Vector3d project_yaw = odom_q.toRotationMatrix().col(0);  // NOTE ZYX
-
+    Eigen::Vector3d project_yaw = odom_q.toRotationMatrix().col(0);
     double now_yaw = std::atan2(project_yaw.y(), project_yaw.x());
     ROS_INFO("now_yaw = %f", now_yaw);
-    if (std::fabs((target_p - odom_p).norm() - tracking_dist_) < tolerance_d_ &&
-        odom_v.norm() < 0.1 && target_v.norm() < 0.2 &&
-        std::fabs(desired_yaw - now_yaw) < 0.5) {
-      if (!wait_hover_) {
-        pub_hover_p(odom_p, ros::Time::now());
-        wait_hover_ = true;
-      }
-      ROS_WARN("[planner] HOVERING...");
-      replanStateMsg_.state = -1;
-      replanState_pub_.publish(replanStateMsg_);
-      return;
-    } else {
-      wait_hover_ = false;
-    }
-
     // NOTE obtain map
     while (gridmap_lock_.test_and_set());
     gridmapPtr_->from_msg(map_msg_);
@@ -365,6 +341,8 @@ class Nodelet : public nodelet::Nodelet {
     target_state.setPos(target_pos);
     controller_.setTargetState(target_state);
     // *********************生成目标物体预测轨迹的部分*********************
+
+    // *********************可视化目标物体维护一个圆范围的部分*********************
     if (generate_new_traj_success) {
       Eigen::Vector3d observable_p = target_predcit.back();
       visPtr_->visualize_path(target_predcit, "car_predict");
@@ -377,30 +355,9 @@ class Nodelet : public nodelet::Nodelet {
       }
       visPtr_->visualize_path(observable_margin, "observable_margin");
     }
+    // *********************可视化目标物体维护一个圆范围的部分*********************
 
     // *********************设置初始状态的部分*********************
-    Eigen::MatrixXd iniState;
-    iniState.setZero(3, 3);
-    ros::Time replan_stamp = ros::Time::now() + ros::Duration(0.03);
-    double replan_t = (replan_stamp - replan_stamp_).toSec();
-    if (force_hover_ || replan_t > traj_poly_.getTotalDuration()) {
-      // 如果不正常状态的话就从上一帧规划的轨迹上拿参数
-      // should replan from the hover state
-      iniState.col(0) = odom_p;
-      iniState.col(1) = odom_v;
-    } else {
-      ROS_INFO("normal state, replan_t = %f", replan_t);
-      // should replan from the last trajectory
-      // 如果正常状态的话就从上一帧规划的轨迹上递推得到初始状态点
-      iniState.col(0) = traj_poly_.getPos(replan_t);
-      iniState.col(1) = traj_poly_.getVel(replan_t);
-      iniState.col(2) = traj_poly_.getAcc(replan_t);
-    }
-    replanStateMsg_.header.stamp = ros::Time::now();
-    replanStateMsg_.iniState.resize(9);
-    Eigen::Map<Eigen::MatrixXd>(replanStateMsg_.iniState.data(), 3, 3) =
-        iniState;
-
     Position start_pos(odom_p.x(), odom_p.y(), odom_p.z());
     Vel start_vel(odom_v.x(), odom_v.y(), odom_v.z());
     State start_state(start_pos, start_vel, now_yaw, odom_omega.z());
@@ -410,193 +367,16 @@ class Nodelet : public nodelet::Nodelet {
     ROS_INFO("init state (vx, vy, vz) = (%f,%f,%f)", odom_v.x(), odom_v.y(),
              odom_v.z());
     ROS_INFO("init state now_yaw= %f", now_yaw);
-    ROS_INFO("init state odom_omega.z()= %f", odom_omega.z());
     // *********************设置初始状态的部分*********************
+
     // *********************kinematic控制的部分*********************
     bool generate_new_point_success = false;
     if (generate_new_traj_success) {
       generate_new_point_success = controller_.Kinematic();
     }
     // *********************kinematic控制的部分*********************
-    // *********************利用A*生成初始轨迹的部分*********************
-    Eigen::Vector3d p_start = iniState.col(0);
-    ROS_INFO("Initial State Matrix:");
-    for (int i = 0; i < iniState.rows(); ++i) {
-      std::ostringstream oss;
-      for (int j = 0; j < iniState.cols(); ++j) {
-        oss << iniState(i, j) << ", ";
-      }
-      ROS_INFO("%s", oss.str().c_str());
-    }
-    std::vector<Eigen::Vector3d> path, way_pts;
 
-    // NOTE calculate time of path searching, corridor generation and
-    // optimization
-
-    // static double t_path_ = 0;
-    // static double t_corridor_ = 0;
-    // static double t_optimization_ = 0;
-    // static int times_path_ = 0;
-    // static int times_corridor_ = 0;
-    // static int times_optimization_ = 0;
-    // double t_path = 0;
-
-    if (generate_new_traj_success) {
-      // ros::Time t_front0 = ros::Time::now();
-      if (land_triger_received_) {
-        generate_new_traj_success =
-            envPtr_->short_astar(p_start, target_p, path);
-      } else {
-        // A*的入口
-        generate_new_traj_success =
-            envPtr_->findVisiblePath(p_start, target_predcit, way_pts, path);
-        ROS_INFO("give A STAR start point (x, y, z) = (%f, %f, %f)",
-                 p_start.x(), p_start.y(), p_start.z());
-        ROS_INFO("target_predcit.size() = %lu", target_predcit.size());
-        ROS_INFO("way_pts.size() = %lu", way_pts.size());
-        ROS_INFO("path.size() = %lu", path.size());
-      }
-      // ros::Time t_end0 = ros::Time::now();
-    }
-    ROS_INFO("generate A* trajectory = %s",
-             generate_new_traj_success ? "true" : "false");
-    // *********************利用A*生成初始轨迹的部分*********************
-
-    std::vector<Eigen::Vector3d> visible_ps;
-    std::vector<double> thetas;
-    Trajectory traj;
-    if (generate_new_traj_success) {
-      visPtr_->visualize_path(path, "astar");
-      if (land_triger_received_) {
-        for (const auto& p : target_predcit) {
-          path.push_back(p);
-        }
-      } else {
-        // *********************生成扇形可视化区域并且生成种子*********************
-        // NOTE generate visible regions
-        target_predcit.pop_back();
-        way_pts.pop_back();
-        ROS_INFO("in visible regions target_predcit.size() = %lu",
-                 target_predcit.size());
-        ROS_INFO("in visible regions way_pts.size() = %lu", way_pts.size());
-        // ros::Time t_front1 = ros::Time::now();
-        envPtr_->generate_visible_regions(target_predcit, way_pts, visible_ps,
-                                          thetas);
-        // 把way_pts作处理得到seed，作为下面的输入，这个seed是根据可视区域得到的位于左右区域上的一个点，
-        // 他的确定原则是基于原来的点位于的区域，而后旋转一定角度得到的。
-        // thetas是左右的角度最大值，visible_ps是角平分线上的距离为允许距离的点
-        // ros::Time t_end1 = ros::Time::now();
-        // t_path += (t_end1 - t_front1).toSec() * 1e3;
-        visPtr_->visualize_pointcloud(visible_ps, "visible_ps");
-        visPtr_->visualize_fan_shape_meshes(target_predcit, visible_ps, thetas,
-                                            "visible_region");
-
-        // TODO change the final state
-        std::vector<std::pair<Eigen::Vector3d, Eigen::Vector3d>> rays;
-        for (int i = 0; i < (int)way_pts.size(); ++i) {
-          rays.emplace_back(target_predcit[i], way_pts[i]);
-        }
-        visPtr_->visualize_pointcloud(way_pts, "way_pts");
-        way_pts.insert(way_pts.begin(), p_start);
-        // ros::Time t_front2 = ros::Time::now();
-        // 把点转化成路径，确保点不碰撞在障碍范围内一定范围内。
-        envPtr_->pts2path(way_pts, path);
-        // ros::Time t_end2 = ros::Time::now();
-        // t_path += (t_end2 - t_front2).toSec() * 1e3;
-      }
-      // *********************生成扇形可视化区域并且生成种子*********************
-      // NOTE corridor generating
-      // *********************生成飞行走廊*********************
-      std::vector<Eigen::MatrixXd> hPolys;
-      std::vector<std::pair<Eigen::Vector3d, Eigen::Vector3d>> keyPts;
-
-      // ros::Time t_front3 = ros::Time::now();
-      envPtr_->generateSFC(path, 2.0, hPolys, keyPts);
-      for (size_t i = 0; i < path.size(); ++i) {
-        const Eigen::Vector3d& vec = path[i];
-        ROS_INFO("A STAR path[%zu]: [%f, %f, %f]", i, vec.x(), vec.y(),
-                 vec.z());
-      }
-      ROS_INFO("hPolys.size() = %lu", hPolys.size());
-      ROS_INFO("keyPts.size() = %lu", keyPts.size());
-      for (size_t i = 0; i < keyPts.size(); ++i) {
-        const Eigen::Vector3d& first = keyPts[i].first;
-        const Eigen::Vector3d& second = keyPts[i].second;
-        ROS_INFO("keyPts[%zu] - First Vector: [%f, %f, %f]", i, first.x(),
-                 first.y(), first.z());
-        ROS_INFO("keyPts[%zu] - Second Vector: [%f, %f, %f]", i, second.x(),
-                 second.y(), second.z());
-      }
-      // ros::Time t_end3 = ros::Time::now();
-      // double t_corridor = (t_end3 - t_front3).toSec() * 1e3;
-
-      envPtr_->visCorridor(hPolys);
-      visPtr_->visualize_pairline(keyPts, "keyPts");
-      // *********************生成飞行走廊*********************
-
-      // *********************轨迹优化部分*********************
-      // NOTE trajectory optimization
-      Eigen::MatrixXd finState;
-      finState.setZero(3, 3);
-      // 末端状态
-      finState.col(0) = path.back();
-      // finState.col(1) = target_v;
-      finState.col(1) = -1.0 * target_v;
-      ROS_INFO("Final State Matrix:");
-      for (int i = 0; i < finState.rows(); ++i) {
-        std::ostringstream oss;
-        for (int j = 0; j < finState.cols(); ++j) {
-          oss << finState(i, j) << ", ";
-        }
-        ROS_INFO("%s", oss.str().c_str());
-      }
-      // ros::Time t_front4 = ros::Time::now();
-
-      // 这个没有被用到
-      if (land_triger_received_) {
-        finState.col(0) = target_predcit.back();
-        generate_new_traj_success = trajOptPtr_->generate_traj(
-            iniState, finState, target_predcit, hPolys, traj);
-      } else {
-        generate_new_traj_success =
-            trajOptPtr_->generate_traj(iniState, finState, target_predcit,
-                                       visible_ps, thetas, hPolys, traj);
-      }
-      // *********************轨迹优化部分*********************
-      // 获取结束时间
-      ros::Time end_time = ros::Time::now();
-      // 计算并输出执行时间
-      ros::Duration execution_time = end_time - start_time;
-      ROS_INFO("plan_timer_callback execution time = %f s",
-               execution_time.toSec());
-      // ros::Time t_end4 = ros::Time::now();
-      // double t_optimization = (t_end4 - t_front4).toSec() * 1e3;
-
-      // NOTE average calculating time of path searching, corridor generation
-      // and optimization
-
-      // t_path_ = (t_path_ * times_path_ + t_path) / (++times_path_);
-      // t_corridor_ = (t_corridor_ * times_corridor_ + t_corridor) /
-      // (++times_corridor_); t_optimization_ = (t_optimization_ *
-      // times_optimization_ + t_optimization) / (++times_optimization_);
-
-      // std::cout << "t_path_: " << t_path_ << " ms" << std::endl;
-      // std::cout << "t_corridor_: " << t_corridor_ << " ms" << std::endl;
-      // std::cout << "t_optimization_: " << t_optimization_ << " ms" <<
-      // std::endl;
-
-      visPtr_->visualize_traj(traj, "traj");
-    }
-    ROS_INFO("generate optimize trajectory = %s",
-             generate_new_traj_success ? "true" : "false");
-    // NOTE collision check
-    bool valid = false;
-    if (generate_new_traj_success) {
-      valid = validcheck(traj, replan_stamp);
-    } else {
-      replanStateMsg_.state = -2;
-      replanState_pub_.publish(replanStateMsg_);
-    }
+    // *********************把点发布出去的部分*********************
     if (generate_new_point_success) {
       Eigen::Vector3d pos(controller_.egoTargetState().pos().x,
                           controller_.egoTargetState().pos().y,
@@ -614,44 +394,8 @@ class Nodelet : public nodelet::Nodelet {
       ROS_INFO("kinematic control omega = %f ", omega);
       pub_point(pos, vel, yaw, omega);
     }
-    // if (valid)
-    if (valid) {
-      force_hover_ = false;
-      ROS_WARN("[planner] REPLAN SUCCESS");
-      replanStateMsg_.state = 0;
-      replanState_pub_.publish(replanStateMsg_);
-      Eigen::Vector3d dp = target_p + target_v * 0.03 - iniState.col(0);
-      // NOTE : if the drone is going to unknown areas, watch that direction
-      // Eigen::Vector3d un_known_p = traj.getPos(1.0);
-      // if (gridmapPtr_->isUnKnown(un_known_p)) {
-      //   dp = un_known_p - odom_p;
-      // }
-      double yaw = std::atan2(dp.y(), dp.x());
-      if (land_triger_received_) {
-        yaw = 2 * std::atan2(target_q.z(), target_q.w());
-      }
-      pub_traj(traj, yaw, replan_stamp);
-      traj_poly_ = traj;
-      replan_stamp_ = replan_stamp;
-    } else if (force_hover_) {
-      ROS_ERROR("[planner] REPLAN FAILED, HOVERING...");
-      replanStateMsg_.state = 1;
-      replanState_pub_.publish(replanStateMsg_);
-      return;
-    } else if (validcheck(traj_poly_, replan_stamp_)) {
-      force_hover_ = true;
-      ROS_FATAL("[planner] EMERGENCY STOP!!!");
-      replanStateMsg_.state = 2;
-      replanState_pub_.publish(replanStateMsg_);
-      pub_hover_p(iniState.col(0), replan_stamp);
-      return;
-    } else {
-      ROS_ERROR("[planner] REPLAN FAILED, EXECUTE LAST TRAJ...");
-      replanStateMsg_.state = 3;
-      replanState_pub_.publish(replanStateMsg_);
-      return;  // current generated traj invalid but last is valid
-    }
-    visPtr_->visualize_traj(traj, "traj");
+    // *********************把点发布出去的部分*********************
+    // *********************kinematic控制的部分*********************
   }
 
   // NOTE main callback 无人机用到的回调函数
